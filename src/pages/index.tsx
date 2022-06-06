6a75cf6996830c9d262ca9a7e1518e89a428ba71
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-06-06 14:50:36 +0600

    Changing api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ const Home: NextPage<{ courses: Course[] }> = ({ courses }) => {
 export default Home;
 
 export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch("https://coursera-app-dashboard.vercel.app/assets/data.json");
+  const res = await fetch("https://coursera-app-dashboard-9adbrebl6-mrhamdan.vercel.app/assets/data.json");
   const courses: Course[] = await res.json();
   return {
     props: {
